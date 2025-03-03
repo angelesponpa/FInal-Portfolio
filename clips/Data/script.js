6305e11771573d6d70ec22a91fa0c455bf1f1ec4
angelesponpa <angelesponpa2025@u.northwestern.edu>
2025-03-02 21:22:48 -0600

    Update script.js

--- clips/Data/script.js
+++ clips/Data/script.js
@@ -1,10 +1,10 @@
 function scrollImages(direction) {
-    const gallery = document.querySelector('.img-container');
-    const images = document.querySelectorAll('.img-container img');
+    const gallery = document.querySelector('.image-gallery');
+    const images = document.querySelectorAll('.image-gallery img');
     const totalImages = images.length;
-    const visibleImages = 2; // Number of images to show at a time
+    const visibleImages = 2;
 
-    currentIndex += direction;
+    currentIndex += direction *visibleImages;
 
     if (currentIndex < 0) {
         currentIndex = 0;
