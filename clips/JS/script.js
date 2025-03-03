cc556b63aea42d8eb10f60cef162b979dbc53655
angelesponpa <angelesponpa2025@u.northwestern.edu>
2025-03-03 00:11:04 -0600

    data page done

--- clips/JS/script.js
+++ clips/JS/script.js
@@ -1,5 +1,5 @@
 function scrollImages(direction) {
-    const gallery = document.querySelector('.image-gallery');
+    const gallery = document.querySelector('.img-container');
     const images = document.querySelectorAll('.image-gallery img');
     const totalImages = images.length;
     const visibleImages = 2;
